a3e3fcabe0f786fd16e50777184515fe8e824225
Brandon Azad <bazad@stanford.edu>
2014-05-24 16:54:17 -0700

    Add support for GHC 7.8

--- src/Crypto/EllipticCurve.hs
+++ src/Crypto/EllipticCurve.hs
@@ -14,9 +14,11 @@ module Crypto.EllipticCurve
     module Crypto.EllipticCurve.Curve
   , module Crypto.EllipticCurve.Group
   , module Crypto.EllipticCurve.Point
+  , module Crypto.EllipticCurve.Type
   ) where
 
 import Crypto.EllipticCurve.Curve
 import Crypto.EllipticCurve.Group
 import Crypto.EllipticCurve.Point
+import Crypto.EllipticCurve.Type
 
